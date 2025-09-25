afb8090993f0bf81544eb69c11311d84b11ddd0b
mekeni <gamao.mary01@gmail.com>
2025-09-25 16:32:11 +0800

    Add files via upload

---
+++ insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO guests (first_name, last_name, email, phone) VALUES ('Daniel', 'Padilla', 'dangwapo143@gmail.com', '09090909091');
+INSERT INTO rooms (room_number, type, price, status) VALUES ('101', 'Single', '1200.00', 'available');
+INSERT INTO reservations (guest_id, room_id, check_in, check_out, total, created_at) VALUES (1, 1, '2025-09-20', '2025-09-22', 2400.00, '2025-09-16 05:41:17');
